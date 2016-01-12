d838f4cab31d954049f46ad5a61645cc07de494b
doloczka <doloczka@gmail.com>
2016-01-12 18:07:42 +0000

    resources student

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
       resources :student
     end
   end
+  resources :student
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
